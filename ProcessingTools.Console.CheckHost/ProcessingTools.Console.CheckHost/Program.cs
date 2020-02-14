0d1b0d8bf6b554ae2620c17a8046b3f2101bb6c4
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2020-02-14 16:01:20 +0200

    Add solution

---
+++ ProcessingTools.Console.CheckHost/ProcessingTools.Console.CheckHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProcessingTools.Console.CheckHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
